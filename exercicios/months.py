b2373ee68739e2561a88f289016825c40e6f7240
ANA PAULA MUELLER DE ALCANTARA <anamueller@gmail.com>
2023-12-03 22:07:09 -0300

    Exercício.

---
+++ exercicios/months.py
@@ -0,0 +1,44 @@
+'''
+    Desafio
+    Leia um valor inteiro entre 1 e 12, inclusive. 
+    Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+    Entrada
+    A entrada contém um único valor inteiro.
+
+    Saída
+    Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.
+
+ 
+    Exemplo de Entrada	Exemplo de Saída
+    4                   April
+'''
+
+''' 
+IMPORTANTE: As funções "input" e "print" são acessíveis nativamente em Python, onde:  
+ - "input": função que permite a leitura de uma entrada do usuário. Lembre-se que, em alguns 
+   casos, é necessário converter/tratar os dados de entrada; 
+ - "print": função que imprime um texto enviado em seu parâmetro, a qual é essencial para a 
+   impressão dos dados de saída. 
+- "dict{}": dicionários possuem uma relação de chave - valor. Para cada chave haverá um valor.
+'''
+
+# Recebe a entrada do usuário e converte para inteiro
+month = int(input())
+
+# Dicionário de meses em inglês
+months_dict = {
+    1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
+    7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
+}
+
+# Verifica se o número está dentro da faixa válida
+if 1 <= month <= 12:
+    # Obtém o nome do mês correspondente do dicionário
+    month_name = months_dict[month]
+
+    # Imprime o nome do mês com a primeira letra em maiúscula
+    print(month_name)
+else:
+    print("Número inválido. Digite um valor entre 1 e 12.")
+
